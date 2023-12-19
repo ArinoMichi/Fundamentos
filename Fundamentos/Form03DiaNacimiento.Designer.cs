269c5967d37f94d56781c28c5547a6b027894fd7
ArinoMichi <cutesyprogrammer@gmail.com>
2023-12-19 14:21:37 +0100

    practice added

--- Fundamentos/Form03DiaNacimiento.Designer.cs
+++ Fundamentos/Form03DiaNacimiento.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form03DiaNacimiento";
+            btnCalcular = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtDia = new TextBox();
+            txtMes = new TextBox();
+            txtAnio = new TextBox();
+            lblResultado = new Label();
+            SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(73, 129);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(128, 49);
+            btnCalcular.TabIndex = 0;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(62, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(24, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Dia";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(56, 47);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Mes";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(55, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(29, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Año";
+            // 
+            // txtDia
+            // 
+            txtDia.Location = new Point(119, 11);
+            txtDia.Name = "txtDia";
+            txtDia.Size = new Size(96, 23);
+            txtDia.TabIndex = 4;
+            // 
+            // txtMes
+            // 
+            txtMes.Location = new Point(119, 40);
+            txtMes.Name = "txtMes";
+            txtMes.Size = new Size(96, 23);
+            txtMes.TabIndex = 5;
+            // 
+            // txtAnio
+            // 
+            txtAnio.Location = new Point(119, 73);
+            txtAnio.Name = "txtAnio";
+            txtAnio.Size = new Size(96, 23);
+            txtAnio.TabIndex = 6;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(36, 201);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(68, 15);
+            lblResultado.TabIndex = 7;
+            lblResultado.Text = "Naciste en: ";
+            // 
+            // Form03DiaNacimiento
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(287, 309);
+            Controls.Add(lblResultado);
+            Controls.Add(txtAnio);
+            Controls.Add(txtMes);
+            Controls.Add(txtDia);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnCalcular);
+            Name = "Form03DiaNacimiento";
+            Text = "Form03DiaNacimiento";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnCalcular;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtDia;
+        private TextBox txtMes;
+        private TextBox txtAnio;
+        private Label lblResultado;
     }
 }
